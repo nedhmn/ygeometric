b035d376a10a0440bad5efe608a66ed9bc4e85bf
nedhmn <hermannned@gmail.com>
2025-02-10 01:02:56 -0500

    feat: vercel-analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,7 @@
+import "@/app/globals.css";
+import { Analytics } from "@vercel/analytics/next";
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
-import "./globals.css";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -55,6 +56,7 @@ export default function RootLayout({
       >
         {children}
       </body>
+      <Analytics />
     </html>
   );
 }
